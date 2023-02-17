5bbf258ade357e5ad8877f806b13edec6c319222
Lee Yejin <lightup.jin@gmail.com>
2023-02-17 11:43:47 +0900

    💄 Added Modal

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   // => state 변동시 자동으로 html에 반영되게 만들고 싶을 때 사용한다.
   // * 자주 변경 되는 html 부문은 state로 만들어 놓기 *
   // Array에서 인덱싱 번호로 원하는 값만 보이게 하기 
-  let [title, content] = useState(['아이스 아메리카노', '카페라떼', '생딸기 라떼']);
+  let [title, content] = useState(['아이스 아메리카노', '카페라떼', '생딸기 라떼', '초코라떼']);
   // 사이트 로고처럼 잘 바뀌지 않는 값은 굳이 state를 사용할 필요 없음. (변수 또는 하드코딩)
   let [logo, setLogo] = useState('React Blog');
   let [like, plus] = useState(0);
@@ -44,11 +44,51 @@ function App() {
   let [a, b] = [1, 2];
   // Array에서 원하는 자료를 뽑는 문법
 
+  // map() 함수 : Array의 갯수만큼 함수안의 코드를 실행 해준다
+  // 콜백 함수 : 소활호 안에 들어가는 함수
+  [1,2,3].map(function(a){
+    // 함수의 파라미터는 array 안에 있는 자료
+    // return에 뭐 적으면 array로 담아준다
+    return '142424'
+    console.log(a)
+  })
+
   // Vanilla JS Code
   // document.querySelector('h1').innerHTML = post;
 
   // 함수 : 긴 Javascript 코드를 한 단어로 묶어주는 문법
   // state 변경은 등호로 하지 않는다.
+
+  // 더러운 HTML 코드를 한 단어로 축약하고 싶을 때 사용
+  // Component 만들기 다른 함수 바깥에 함수 만들기 & 대문자
+  // return() 안에 html담기
+  // return 안에서는 하나의 태그로 시작해서 하나의 태그로 끝나야 한다
+  // 두개의 div를 병렬적으로 넣으면 안됨
+  // 오른쪽에 state변경 함수는 앞에 set을 붙여서 작명하는게 일반적인 관습. (필수 X)
+  // UI의 현재 상태를 state로 저장 => 형식은 자유(모달창 상태만 표현 가능하면 됨)
+  let [modal, setModal] = useState(false);
+  function Modal() {
+    return (
+      //  div들을 하나로 묶어야할 때 의미없는 div대신 <> </> 사용가능
+      <>
+        <div className="modal">
+          <h4>제목 </h4>
+          <p>날짜</p>
+          <p>상세내용</p>
+        </div>
+        <div></div>
+      </>
+    )
+  }
+
+  function Index() {
+    return (
+      <div className="list">
+        <h4 onClick={()=>{setModal(!modal)}}>{title[0]}</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    )
+  }
 
   return (
     <div className="App">
@@ -89,9 +129,46 @@ function App() {
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
       <div className="list">
-        <h4>{title[2]}</h4>
+        {/* modal = useState(true); 동작하지 않는 이유 => state 변경함수 사용 */}
+        <h4 onClick={() => {setModal(!modal)}}>{title[2]}</h4>
+        {
+          modal == true ? <Modal/> : null
+        }
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
+      <Index/>
+      {
+        modal == true ? <Modal/> : null
+      }
+      <Index/>
+      {/* React Component 
+        어떤걸 컴포넌트로 만들면 좋은가?
+        1. 반복적인 html 축약할 때
+        2. 큰 페이지들
+        3. 자주 변경되는 HTML UI
+      */}
+
+      {/* <Modal></Modal> */}
+      {
+        // HTML 중간에 조건문 쓰려면 삼항= 연산자"로 작성 해야함.
+        // 조건식 ? 참일 때 실행 코드 : 거짓일 때 실행 코드
+        // 1 == 2 ? '맞음' : '아님'
+        // modal == true ? <Modal/> : null
+      }
+
+      {
+        // 두번째 파라미터는 반복문이 돌 때 마다 0부터 1씩 증가하는 정수
+        title.map(function(a, i){
+          return (
+            <div className="list">
+              {/* <h4>{a}</h4> */}
+              <h4>{title[i]}</h4>
+              <p>02월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+
       {/* <h1>{post}</h1> */}
       <button className='array-btn' onClick={()=>{
         let arrayCopy = [...title];
